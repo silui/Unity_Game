c10e4ae17f2fadaa259c1ce8f9387b8ad887466f
edwardlui531 <edwardlui531@gmail.com>
2019-09-16 23:25:26 -0700

    added Stroke_classifier

---
+++ Stroke_classifier.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+public class Stroke_classifier : knn
+{
+    private List<int> classes = new List<int>();
+    private int max_sample;
+    private bool trained = false;
+    private int min_Stroke_Point = 5;
+
+    private List<double> xyratio = new List<double>();
+
+    public Stroke_classifier(int classcount, int sample_count)
+    {
+        for (int i=0; i<classcount; i++)
+        {
+            classes.Add(i);
+        }
+        max_sample = sample_count;
+
+    }
+    public void addStroke(List<Vector2> inputStroke)
+    {
+        inputStroke = removeConsectiveRepeat(inputStroke);
+        if(inputStroke.Count<min_Stroke_Point)
+        {
+            Debug.Log("Stroke_rejected");
+            return;
+        }
+        xyratio.Add(xyratio_cal(inputStroke));
+    }
+
+
+    private List<Vector2> removeConsectiveRepeat(List<Vector2> inputStroke)
+    {
+        List<Vector2> returnThis = new List<Vector2>();
+        foreach(Vector2 point in inputStroke)
+        {
+            if(returnThis.Count==0 || point != returnThis.Last())
+            {
+                returnThis.Add(point);
+            }
+        }
+        return returnThis;
+    }
+
+    private double xyratio_cal(List<Vector2> inputStroke)
+    {
+        double x_min = double.PositiveInfinity;
+        double x_max = double.NegativeInfinity;
+        double y_min = double.PositiveInfinity;
+        double y_max = double.NegativeInfinity;
+        foreach(Vector2 point in inputStroke)
+        {
+            x_min = Math.Min(point.x, x_min);
+            x_max = Math.Max(point.x, x_max);
+            y_min = Math.Min(point.y, y_min);
+            y_max = Math.Max(point.y, y_max);
+        }
+        return (x_max-x_min)/(y_max-y_min);
+
+    }
+}
